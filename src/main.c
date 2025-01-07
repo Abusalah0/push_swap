ee8348fa40c42babd505786a3ce29c19b013f5ac
Abusalah0 <109g83@gmail.com>
2025-01-07 17:41:31 +0300

    more robust

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abdsalah <abdsalah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 21:39:06 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/01/07 14:35:57 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/01/07 17:33:33 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,11 @@ int	main(int argc, char **argv)
 	b_stack = NULL;
 	num = check_input(argc, argv);
 	if (!num)
-	{
-		printf("Error\n");
-		return (1);
-	}
+		error_exit("ERROR\n");
 	fill_stack(&a_stack, num, argc - 1);
-	sort(&a_stack, &b_stack);
 	free(num);
+	sort(&a_stack, &b_stack);
+	free_stack(a_stack);
+	free_stack(b_stack);
 	return (0);
 }

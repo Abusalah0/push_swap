3d87d31bec3000edcad176ed1becdeee2f5d4b6e
Abusalah0 <109g83@gmail.com>
2025-01-07 19:40:47 +0300

    norminette, corrected some shit

--- src/hashset.c
+++ src/hashset.c
@@ -6,12 +6,11 @@
 /*   By: abdsalah <abdsalah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 19:18:26 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/01/07 19:19:01 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/01/07 19:40:04 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
-
 
 static unsigned int	hash(int key, unsigned int size)
 {
@@ -20,12 +19,28 @@ static unsigned int	hash(int key, unsigned int size)
 	return (key % size);
 }
 
+static int	insert(int *hashset, unsigned int size, int value)
+{
+	unsigned int	index;
+
+	index = hash(value, size);
+	while (hashset[index] != 0)
+	{
+		if (hashset[index] == value)
+		{
+			return (true);
+		}
+		index = (index + 1) % size;
+	}
+	hashset[index] = value;
+	return (false);
+}
+
 bool	check_for_dups(int *nums, int nums_size)
 {
 	int				i;
 	unsigned int	setsize;
 	int				*hashset;
-	unsigned int	index;
 
 	i = -1;
 	setsize = nums_size * 2 + 1;
@@ -34,17 +49,11 @@ bool	check_for_dups(int *nums, int nums_size)
 		return (true);
 	while (++i < nums_size)
 	{
-		index = hash(nums[i], setsize);
-		while (hashset[index] != 0)
+		if (insert(hashset, setsize, nums[i]))
 		{
-			if (hashset[index] == nums[i])
-			{
-				free(hashset);
-				return (true);
-			}
-			index = (index + 1) % setsize;
+			free(hashset);
+			return (true);
 		}
-		hashset[index] = nums[i];
 	}
 	free(hashset);
 	return (false);

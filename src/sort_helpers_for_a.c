59995ed1b53261f6cceee4a138cf6c00f34cb797
Abusalah0 <109g83@gmail.com>
2025-01-09 03:54:43 +0300

    better sort logic

--- src/sort_helpers_for_a.c
+++ src/sort_helpers_for_a.c
@@ -6,11 +6,34 @@
 /*   By: abdsalah <abdsalah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 21:17:32 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/01/08 06:05:19 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/01/09 03:54:06 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+
+/*
+** cost_helper:
+**--------------------------
+** Set the push cost for each node in stack A
+*/
+static void cost_helper(t_item *a_stack, int a_size, int b_size)
+{
+	if (a_stack->above_median)
+	{
+		if (a_stack->index > a_stack->target->index)
+			a_stack->push_cost = a_stack->index;
+		else
+			a_stack->push_cost = a_stack->target->index;
+	}
+	else
+	{
+		if (a_size - a_stack->index > b_size - a_stack->target->index)
+			a_stack->push_cost = a_size - a_stack->index;
+		else
+			a_stack->push_cost = b_size - a_stack->target->index;
+	}
+}
 
 void	set_cost(t_item *a_stack, t_item *b_stack)
 {
@@ -21,13 +44,21 @@ void	set_cost(t_item *a_stack, t_item *b_stack)
 	b_size = get_size(b_stack);
 	while (a_stack)
 	{
-		a_stack->push_cost = a_size - a_stack->index;
-		if (!a_stack->above_median)
-			a_stack->push_cost = a_size - a_stack->index;
-		if (a_stack->target->above_median)
-			a_stack->push_cost += a_stack->target->index;
+		if (a_stack->above_median == a_stack->target->above_median)
+		{
+			cost_helper(a_stack, a_size, b_size);
+		}
 		else
-			a_stack->push_cost += b_size - (a_stack->target->index);
+		{
+			if (a_stack->above_median)
+				a_stack->push_cost = a_stack->index;
+			else
+				a_stack->push_cost = a_size - a_stack->index;
+			if (a_stack->target->above_median)
+				a_stack->push_cost += a_stack->target->index;
+			else
+				a_stack->push_cost += b_size - a_stack->target->index;
+		}
 		a_stack = a_stack->next;
 	}
 }
@@ -40,18 +71,21 @@ void	set_target_node_a(t_item *a_stack, t_item *b_stack)
 
 	while (a_stack)
 	{
-		closest_match = LONG_MIN;
+		closest_match = LONG_MAX;
 		curser = b_stack;
 		while (curser)
 		{
-			if (curser->val < a_stack->val && curser->val > closest_match)
+			if (curser->val < a_stack->val)
 			{
-				closest_match = curser->val;
-				target = curser;
+				if (LONG_MAX == closest_match || curser->val > closest_match)
+				{
+					closest_match = curser->val;
+					target = curser;
+				}
 			}
 			curser = curser->next;
 		}
-		if (closest_match == LONG_MIN)
+		if (closest_match == LONG_MAX)
 			a_stack->target = get_max(b_stack);
 		else
 			a_stack->target = target;
